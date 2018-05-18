d5fd00e92137d03a050ef0c5ad8e13700999b081
FooTage <pookair@gmail.com>
2018-05-18 16:03:02 +0300

    simple overlay start

--- src/dominantColor.py
+++ src/dominantColor.py
@@ -6,23 +6,24 @@ import scipy.misc
 import scipy.cluster
 def dominantColor(filename):
     NUM_CLUSTERS = 3
-    print ('reading image')
-    im = PIL.Image.open(filename)
-    im = im.resize((50, 50))      # optional, to reduce time
+    #print ('reading image')
+    im = PIL.Image.fromarray(filename)
+    #im = PIL.Image.open(filename)
+    im = im.resize((25, 25))      # optional, to reduce time
     ar = np.asarray(im)
     shape = ar.shape
     ar = ar.reshape(scipy.product(shape[:2]), shape[2]).astype(float)
 
-    print ('finding clusters')
+    #print ('finding clusters')
     codes, dist = scipy.cluster.vq.kmeans(ar, NUM_CLUSTERS)
-    print ('cluster centres:\n', codes)
+    #print ('cluster centres:\n', codes)
 
     vecs, dist = scipy.cluster.vq.vq(ar, codes)         # assign codes
     counts, bins = scipy.histogram(vecs, len(codes))    # count occurrences
 
     index_max = scipy.argmax(counts)                    # find most frequent
     peak =  codes[index_max]
-    print('max_val:\n', peak)
+    #print('max_val:\n', peak)
     return peak
     #peak = codes[index_max]
     #colour = ''.join(chr(int(c)) for c in peak).encode('hex')
--- src/main.py
+++ src/main.py
@@ -3,6 +3,22 @@
 
 import numpy as np
 import cv2 as cv
+from dominantColor import dominantColor
+
+class ParkingSpace():
+    def __init__(self, x1, x2, y1, y2):
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+        self.emtpy = True
+
+    def getCrop(self, frame):
+        return frame[self.y1:self.y2, self.x1:self.x2]
+
+parkingSpaces = [ParkingSpace(808, 886, 469, 527)]
+
+
 
 cap = cv.VideoCapture('../work.flv')
 while(cap.isOpened()):
@@ -12,12 +28,16 @@ while(cap.isOpened()):
         print("end of stream")
         break
     
-    #gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    for pSpace in parkingSpaces:
+        dcolor = dominantColor(pSpace.getCrop(frame))
+        cv.rectangle(frame, (pSpace.x1, pSpace.y1), (pSpace.x2, pSpace.y2),
+		(dcolor[0], dcolor[1], dcolor[2]), -1)
     #cut frame [y1:y2, x1:x2]
-    crop_img = frame[469:527, 808:886]
+    #crop_img = frame[469:527, 808:886]
     #
 
-    cv.imshow('frame',crop_img)
+    cv.imshow('frame',frame)
+    #cv.imshow('crop',crop_img)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
